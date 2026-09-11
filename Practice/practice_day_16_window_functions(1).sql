31944e045296670ad3fa1e3db50186b53190a402
Himani-S-Devadiga <himanisdevadiga59@gmail.com>
2026-09-11 23:39:08 +0530

    Update and rename practice_day_16_window_functions.sql to practice_day_16_window_functions(1).sql

--- Practice/practice_day_16_window_functions(1).sql
+++ Practice/practice_day_16_window_functions(1).sql
@@ -1,5 +1,5 @@
 =====================================
-     Window Practice (Day-1)
+    Basic Window Practice (Day-1)
 =====================================
 1.Write a query to display emp_name, salary, and the overall average salary of all employees beside every employee.
 SELECT emp_name,salary,
@@ -10,3 +10,9 @@ FROM employees;
   SELECT emp_name,department_id,salary,
   AVG(salary) OVER(PARTITION BY department_id) AS dept_avg
   FROM employees;
+
+3.Display each employee's emp_name, department_id, salary, and the highest salary in their department.
+SELECT emp_name,department_id,salary,
+MAX(salary) OVER(PARTITION BY department_id) AS dept_max
+FROM employees;
+
